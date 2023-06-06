83f30dc3f9ecbc1681d663173c448c95130459b1
Joshua Martin <josh.martin@cumulusops.com>
2023-06-06 11:21:19 -0400

    refactored and cleaned up

--- basic_functions.py
+++ basic_functions.py
@@ -1,3 +1,8 @@
 def add(a, b):
     """Add two numbers together"""
-    return a + b
+    return a + b - 1  # Intentional bug
+
+
+def multiply(a, b):
+    """Multiply two numbers together"""
+    return a * b
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,11 @@
-from basic_functions import add
+from basic_functions import add, multiply
 
 
 def test_add():
     """Test the add function"""
     assert add(2, 3) == 5
+
+
+def test_multiply():
+    """Test the multiply function"""
+    assert multiply(2, 3) == 6
